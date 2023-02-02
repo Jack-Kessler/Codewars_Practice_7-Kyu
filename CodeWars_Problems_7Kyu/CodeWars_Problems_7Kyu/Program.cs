cd4202abcb2e2b235843683ef2a0744ec3384432
Jack Kessler <jack_kessler@hotmail.com>
2023-02-01 20:51:49 -0600

    Three 7-Kyus Down

--- CodeWars_Problems_7Kyu/CodeWars_Problems_7Kyu/Program.cs
+++ CodeWars_Problems_7Kyu/CodeWars_Problems_7Kyu/Program.cs
@@ -1,4 +1,60 @@
-﻿//Sum of Integers in String
+﻿//Cats and Shelves (7-Kyu)
+
+//int start = 2;
+//int finish = 5;
+//int diffStartFinish = finish - start;
+//int bigJump = 3;
+//int remainder = 0;
+//int quot = Math.DivRem(diffStartFinish, bigJump, out remainder);
+//int totalJumps = quot + remainder;
+
+//Console.WriteLine(totalJumps);
+
+//return totalJumps;
+
+//-----------------------------------------------------------------------------------------------------------------------------
+//Largest Elements (7-Kyu)
+
+//using System.Collections.Generic;
+
+//int n = 2;
+
+//List<int> xs = new List<int>() { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }; // Fast Method
+
+//List<int> exampleNumbers = new List<int>(); // Slow Method
+//xs.Add(10);
+//xs.Add(9);
+//xs.Add(8);
+//xs.Add(7);
+//xs.Add(6);
+//xs.Add(5);
+//xs.Add(4);
+//xs.Add(3);
+//xs.Add(2);
+//xs.Add(1);
+
+//List<int> finalList = new List<int>();
+
+//for (int w = 1; w <= n; w++)
+//{
+//    int s = xs.Max();
+//    finalList.Add(s);
+//    xs.Remove(s);
+//}
+//finalList.Reverse();
+
+//foreach (int i in finalList) // for VS
+//{
+//    Console.WriteLine(i);
+//}
+
+//return List; for codewars
+
+//return xs.OrderBy(x => x).TakeLast(n).ToList();
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+//Sum of Integers in String (7-Kyu)
 //using System.Linq.Expressions;
 
 //string s = "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog";
